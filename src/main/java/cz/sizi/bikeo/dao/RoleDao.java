7b076bf3603ee7a2dd905d8176bffee79e54a358
Unknown <mich.sipek@gmail.com>
2016-10-30 21:48:37 +0100

    Implemented RoleDao methods

--- src/main/java/cz/sizi/bikeo/dao/RoleDao.java
+++ src/main/java/cz/sizi/bikeo/dao/RoleDao.java
@@ -3,5 +3,6 @@ package cz.sizi.bikeo.dao;
 import cz.sizi.bikeo.model.Role;
 
 public interface RoleDao extends GenericDao<Role, Integer> {
+	Role findByRoleName(String roleName);
 
 }
--- src/main/java/cz/sizi/bikeo/dao/impl/RoleDaoImpl.java
+++ src/main/java/cz/sizi/bikeo/dao/impl/RoleDaoImpl.java
@@ -1,7 +1,11 @@
 package cz.sizi.bikeo.dao.impl;
 
+import java.io.Serializable;
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import cz.sizi.bikeo.dao.RoleDao;
@@ -10,34 +14,47 @@ import cz.sizi.bikeo.model.Role;
 @Repository
 public class RoleDaoImpl implements RoleDao {
 
+	private SessionFactory sessionFactory;
+
+	@Autowired
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<Role> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		return sessionFactory.getCurrentSession().createQuery("from Role").list();
 	}
 
 	@Override
-	public Role save(Role t) {
-		// TODO Auto-generated method stub
-		return null;
+	public void save(Role role) {
+		sessionFactory.getCurrentSession().save(role);
 	}
 
 	@Override
-	public Role update(Role t) {
-		// TODO Auto-generated method stub
-		return null;
+	public Role update(Role role) {
+		sessionFactory.getCurrentSession().update(role);
+		return role;
 	}
 
 	@Override
-	public void remove(Role t) {
-		// TODO Auto-generated method stub
-		
+	public void remove(Role role) {
+		sessionFactory.getCurrentSession().delete(role);
 	}
 
 	@Override
 	public Role findById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		return (Role) sessionFactory.getCurrentSession().get(Role.class, (Serializable) id);
+	}
+
+	@Override
+	public Role findByRoleName(String roleName) {
+		Query query = sessionFactory.getCurrentSession().createQuery("from Role where name = :roleName");
+		query.setParameter("roleName", roleName);
+		Role role = (Role) query.uniqueResult();
+		return role;
+
 	}
 
 }
